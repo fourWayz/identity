ee9312985d2b0be341a857216aa1001f1fc0f9db
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-06-12 12:17:05 +0000

    write tests and set up config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,29 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('@nomicfoundation/hardhat-toolbox')
+require('@nomicfoundation/hardhat-chai-matchers')
+require('hardhat-deploy')
+require('dotenv').config()
+
+const wallet_private_key = process.env.wallet_private_key // private key
+const sepolia_rpc = process.env.sepolia_rpc // rpc url
+
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+      hardhat: {
+          chainId: 31337,
+      },
+     sepolia :{
+      url : sepolia_rpc,
+      chainId : 11155111,
+      accounts :[wallet_private_key]
+     }
+    },
+    namedAccounts: {
+      deployer: {
+          default: 0, 
+          1: 0, 
+      },     
+  },
 };
---
+++ test/identityVerifcation.test.js
@@ -0,0 +1,42 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+
+describe("IdentityVerification", function () {
+  let IdentityVerification;
+  let identityVerification;
+  let admin;
+  let verifier;
+  let user;
+
+  beforeEach(async function () {
+    [admin, verifier, user, _] = await ethers.getSigners();
+    IdentityVerification = await ethers.getContractFactory("IdentityVerification");
+    identityVerification = await IdentityVerification.deploy();
+    await identityVerification.deployed();
+    await identityVerification.assignRole(verifier.address, 2); // 2 is Verifier role
+  });
+
+  it("Should assign roles correctly", async function () {
+    expect(await identityVerification.roles(verifier.address)).to.equal(2); // 2 is Verifier role
+  });
+
+  it("Should add, update, verify, revoke, and delete identity", async function () {
+    await identityVerification.addIdentity(user.address, "Alice", "alice@example.com");
+    let identity = await identityVerification.getIdentity(user.address);
+    expect(identity.name).to.equal("Alice");
+
+    await identityVerification.updateIdentity(user.address, "Alice Updated", "alice_updated@example.com");
+    identity = await identityVerification.getIdentity(user.address);
+    expect(identity.name).to.equal("Alice Updated");
+
+    await identityVerification.connect(verifier).verifyIdentity(user.address);
+    expect(await identityVerification.isIdentityVerified(user.address)).to.be.true;
+
+    await identityVerification.revokeIdentity(user.address);
+    expect(await identityVerification.isIdentityVerified(user.address)).to.be.false;
+
+    await identityVerification.deleteIdentity(user.address);
+    await expect(identityVerification.getIdentity(user.address)).to.be.revertedWith("Identity does not exist");
+  });
+});
